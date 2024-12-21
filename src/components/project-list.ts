1107ce7258bdeb89a505d07d6056beed3feeb554
Matilda-bit <polinaovras@gmail.com>
2024-12-22 00:35:50 +0200

    install webpack, added entry & output configuration

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,11 +1,11 @@
 
 
-import { DragTarget } from '../models/drag-drop.js';
-import { Component } from './base-component.js';
-import { Project, ProjectStatus } from '../models/project.js';
-import { projectState } from '../state/project-state.js';
-import { autobind } from '../decorators/autobind.js';
-import { ProjectItem } from './project-item.js';
+import { DragTarget } from '../models/drag-drop';
+import { Component } from './base-component';
+import { Project, ProjectStatus } from '../models/project';
+import { projectState } from '../state/project-state';
+import { autobind } from '../decorators/autobind';
+import { ProjectItem } from './project-item';
 
     // ProjectList Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget{
